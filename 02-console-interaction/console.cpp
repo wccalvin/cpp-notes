8ac0478f5124011b65a8b41da37487be1b097cbe
Clayton William <clayton.calvin@gmail.com>
2022-06-14 07:57:19 -0400

    feat: refactor cin example

--- 02-console-interaction/console.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-  std::string str;
-  std::cin >> str;
-  std::cout << str << std::endl;
-}
---
+++ 02-console/console.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  int age;
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+  std::cout << "You are " << age << " years old." << std::endl;
+}
