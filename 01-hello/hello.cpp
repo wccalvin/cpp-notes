9ed702db3e0cc648021b104ebd1d4a145d143480
wccalvin <clayton.calvin@gmail.com>
2022-06-14 13:26:55 -0400

    first program example

--- 01-hello/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello there, first c++ program" << std::endl;
-  return (0);
-}
---
+++ 01-hello/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// first program
+int main() {
+  std::cout << "hello, world!" << std::endl;
+  return 0;
+}
