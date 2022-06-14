6f3a432aaf8acbc60db04ef61888c3e7c55852c1
wccalvin <clayton.calvin@gmail.com>
2022-06-14 16:20:05 -0400

    example for cin

--- 02-console/console.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-  int age;
-  std::cout << "Enter your age: ";
-  std::cin >> age;
-  std::cout << "You are " << age << " years old." << std::endl;
-}
---
+++ 02-console/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello, " << name << "!" << std::endl;
+}
