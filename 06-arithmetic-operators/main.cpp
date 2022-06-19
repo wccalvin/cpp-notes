c96c30800d6efbbb4c0b7d2eeecd629313cde755
Clayton William <clayton.calvin@gmail.com>
2022-06-19 00:00:19 -0400

    example of arithmetic operations

---
+++ 06-arithmetic-operators/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+  int a = 10;
+  int b = 12;
+
+  // add
+  int sum = a + b;
+  std::cout << "a + b = " << sum << std::endl;
+
+  // subtract
+  int diff = b - a;
+  std::cout << "b - a = " << diff << std::endl;
+
+  // multiply
+  int product = a * b;
+  std::cout << "a * b = " << product << std::endl;
+
+  // divide
+  int quotient = b / a;
+  std::cout << "b / a = " << quotient << std::endl;
+
+  // modulus
+  int remainder = b % a;
+  std::cout << "b % a = " << remainder << std::endl;
+
+  return 0;
+}
