0a9e96813ac6921a779c0d0e373fb08758c70217
Clayton William <clayton.calvin@gmail.com>
2022-06-18 22:32:01 -0400

    data type examples

---
+++ 04-data-types/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // int
+  int num = 4;
+  std::cout << "type: int; value: " << num << std::endl;
+
+  // double
+  double price = 1.99;
+  std::cout << "type: double; value: " << price << std::endl;
+
+  // char
+  char letter = 'a';
+  std::cout << "type: char; value: " << letter << std::endl;
+
+  // string
+  std::string name = "john doe";
+  std::cout << "type: string; value: " << name << std::endl;
+
+  // boolean
+  bool is_exist = true;
+  is_exist = false;
+  std::cout << "type: bool; value: " << is_exist << std::endl;
+
+  return 0;
+}
