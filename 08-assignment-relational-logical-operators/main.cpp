b61d3e259c77e572cffdd1192f3ddaed8152987f
wccalvin <clayton.calvin@gmail.com>
2022-06-27 09:50:03 -0400

    example for relational ops

--- 08-assignment-relational-logical-operators/main.cpp
+++ 08-assignment-relational-logical-operators/main.cpp
@@ -10,5 +10,13 @@ int main() {
   y *= 20;
   std::cout << "x += 2: " << x << std::endl;
   std::cout << "y *= 20: " << y << std::endl;
+
+  // relational operators
+  int a = 7, b = 5, c = 7;
+  std::cout << "a > b: " << (a > b) << std::endl;    // true (1)
+  std::cout << "a != c: " << (a != c) << std::endl;  // false (0)
+  std::cout << "a == c: " << (a == c) << std::endl;  // true (1)
+  std::cout << "b >= a: " << (b >= a) << std::endl;  // false (0)
+
   return 0;
 }
