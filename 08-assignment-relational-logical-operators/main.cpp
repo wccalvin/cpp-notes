3b17cdcf06b00d6d714e5167684155cb5d7171d8
wccalvin <clayton.calvin@gmail.com>
2022-06-27 09:57:21 -0400

    example for logical operators

--- 08-assignment-relational-logical-operators/main.cpp
+++ 08-assignment-relational-logical-operators/main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
   // assignment operators
+  std::cout << "--- assignment operators --- " << std::endl;
   int x = 10;
   int y = 5;
   std::cout << "x: " << x << std::endl;
@@ -12,11 +13,20 @@ int main() {
   std::cout << "y *= 20: " << y << std::endl;
 
   // relational operators
+  std::cout << "--- relational operators --- " << std::endl;
   int a = 7, b = 5, c = 7;
   std::cout << "a > b: " << (a > b) << std::endl;    // true (1)
   std::cout << "a != c: " << (a != c) << std::endl;  // false (0)
   std::cout << "a == c: " << (a == c) << std::endl;  // true (1)
   std::cout << "b >= a: " << (b >= a) << std::endl;  // false (0)
+
+  // logical operators
+  std::cout << "--- logical operators --- " << std::endl;
+  bool is_true = true, is_false = false;
+  std::cout << "true && false: " << (is_true && is_false) << std::endl;      // false (0)
+  std::cout << "true || false: " << (is_true || is_false) << std::endl;      // true (1)
+  std::cout << "!true: " << (!is_true) << std::endl;                         // false (0)
+  std::cout << "!(true && false): " << !(is_true && is_false) << std::endl;  // true (1)
 
   return 0;
 }
