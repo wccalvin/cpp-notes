3e9deff69de31b150ec1521402bd234afbecb4d6
Clayton William <clayton.calvin@gmail.com>
2022-08-27 10:03:45 -0400

    refact: default constructor example

--- 17-oop-constructors/main.cpp
+++ 17-oop-constructors/main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <string>
+class Car {
+ private:
+  std::string make;
+  std::string model;
+  int horse_power;
+
+ public:
+  // Default constructor
+  Car() {
+    make = "Subaru";
+    model = "WRX";
+    horse_power = 258;
+  }
+
+  void summary() {
+    std::cout << make + " " + model + " has " << horse_power << " hp." << std::endl;
+  }
+};
 
 int main() {
-  class Car {
-   private:
-    std::string make;
-    std::string model;
-    int horse_power;
-
-   public:
-    // Default constructor
-    Car() {
-      make = "Subaru";
-      model = "WRX";
-      horse_power = 258;
-    }
-
-    void summary() {
-      std::cout << make + " " + model + " has " << horse_power << " hp." << std::endl;
-    }
-  };
-
   // object using default constructor
   Car wrx;
   wrx.summary();
