6162f45daf51365dc9968b8b289209d7b75a8e74
wccalvin <clayton.calvin@gmail.com>
2022-09-23 11:59:11 -0400

    data types example

--- 04-data-types/main.cpp
+++ 04-data-types/main.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
-  // int
-  int num = 4;
-  std::cout << "type: int; value: " << num << std::endl;
+    // int
+    int num = 4;
+    cout << "type: int; value: " << num << endl;
 
-  // double
-  double price = 1.99;
-  std::cout << "type: double; value: " << price << std::endl;
+    // unsigned int - only stores 0 and +ve integers
+    unsigned int positive = 1;
+    cout << "type: unsigned int; value: " << positive << endl;
 
-  // char
-  char letter = 'a';
-  std::cout << "type: char; value: " << letter << std::endl;
+    // unsigned long int
+    unsigned long population_of_usa = 329500000;
+    cout << "type: unsigned long int; value: " << population_of_usa << endl;
 
-  // string
-  std::string name = "john doe";
-  std::cout << "type: string; value: " << name << std::endl;
+    // long long
+    long long population_on_earth{7'600'000'000};  // C++ 14 feature - single quote for legibility
+    cout << "type: long long int; value: " << population_on_earth << endl;
 
-  // boolean
-  bool is_exist = true;
-  is_exist = false;
-  std::cout << "type: bool; value: " << is_exist << std::endl;
+    // float
+    float car_payment = 469.23;
+    cout << "type: float; value: " << car_payment << endl;
 
-  // type conversion double to int
-  double value = 33.55;
-  int converted_value = (int)value;
+    // double
+    double pi = 3.141592653589;
+    cout << "type: double; value: " << pi << endl;
 
-  std::cout << value << " converted to " << converted_value << std::endl;
+    long double long_value = 1.5e10;
+    cout << "type: long double; value: " << long_value << endl;
 
-  return 0;
+    // char
+    char letter = 'a';
+    cout << "type: char; value: " << letter << endl;
+
+    // string
+    string name = "john doe";
+    cout << "type: string; value: " << name << endl;
+
+    // boolean
+    bool is_exist = true;
+    bool is_not_exist = false;
+    cout << "type: bool (true); value: " << is_exist << endl;
+    cout << "type: bool (false); value: " << is_not_exist << endl;
+
+    // type conversion double to int
+    double value = 33.55;
+    int converted_value = (int)value;
+
+    cout << value << " converted to " << converted_value << endl;
+
+    return 0;
 }
