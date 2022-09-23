b568166f139eb3d73947a458b3a238d88a30ddf2
wccalvin <clayton.calvin@gmail.com>
2022-09-23 13:56:14 -0400

    add sizeof examples

--- 04-data-types/main.cpp
+++ 04-data-types/main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 #include <string>
 
@@ -50,6 +51,39 @@ int main() {
     int converted_value = (int)value;
 
     cout << value << " converted to " << converted_value << endl;
+
+    // sizeof operator - determines the size in bytes
+    cout << "------ sizeof info -------\n";
+    cout << "=== int ===\n";
+    cout << "sizeof of int: " << sizeof(int) << " bytes" << endl;
+    cout << "sizeof of unsigned int: " << sizeof(unsigned int) << " bytes" << endl;
+    cout << "sizeof of signed int: " << sizeof(signed int) << " bytes" << endl;
+    cout << "sizeof of short int: " << sizeof(short int) << " bytes" << endl;
+    cout << "sizeof of long int: " << sizeof(long int) << " bytes" << endl;
+    cout << "sizeof of long long int: " << sizeof(long long int) << " bytes" << endl;
+    cout << "=== float ===\n";
+    cout << "sizeof of float: " << sizeof(float) << " bytes" << endl;
+    cout << "sizeof of double: " << sizeof(double) << " bytes" << endl;
+    cout << "sizeof of long double: " << sizeof(long double) << " bytes" << endl;
+    cout << "=== char ===\n";
+    cout << "sizeof of char: " << sizeof(char) << " bytes" << endl;
+    cout << "=== string ===\n";
+    cout << "sizeof of char: " << sizeof(string) << " bytes" << endl;
+
+    // use values defined in climits
+    cout << "=== climits minimum values ===\n";
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short int: " << SHRT_MIN << endl;
+    cout << "long int: " << LONG_MIN << endl;
+    cout << "long long int: " << LONG_LONG_MIN << endl;
+
+    cout << "=== climits max values ===\n";
+    cout << "char: " << CHAR_MAX << endl;
+    cout << "int: " << INT_MAX << endl;
+    cout << "short int: " << SHRT_MAX << endl;
+    cout << "long int: " << LONG_MAX << endl;
+    cout << "long long int: " << LONG_LONG_MAX << endl;
 
     return 0;
 }
