cca2242ac38d254e62c6006d2ff0c10ca0f9cea2
wccalvin <clayton.calvin@gmail.com>
2022-10-25 16:25:56 -0400

    example for references

--- 09-references/main.cpp
+++ 09-references/main.cpp
@@ -1,22 +1,81 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 /*
 - reference is an alias to an existing variable.
 - it is declared using &
 - references must be initialized.
 - references cannot be reassigned.
+- think of reference as a constant pointer that is automatically dereferenced.
 */
 
-int main() {
-  int x = 7;
-  int& y = x;
-  std::cout << "x: " << x << " and reference y: " << y << std::endl;
+// function prototype
+void display_vector(vector<string>);
 
-  // changing x will also change y
-  x = 5;
-  std::cout << "x: " << x << " and reference y: " << y << std::endl;
+int main()
+{
+  cout << "-- intialize variable and reference --\n";
+  int x{7};
+  int &xref = x;
+  cout << "x: " << x << endl;
+  cout << "xref: " << xref << endl; // no dereferening needed
 
-  // print the memory address of variables
-  std::cout << "x memory address: " << &x << " and reference y memory address: " << &y << std::endl;
+  // changing x will also change xref
+  cout << "\n-- reassign values through variable --\n";
+  x = 22;
+  cout << "x after: " << x << endl;
+  cout << "xref after: " << xref << endl;
+
+  cout << "\n-- reassign values through reference --\n";
+  xref = 9;
+  cout << "x after: " << x << endl;
+  cout << "xref after: " << xref << endl;
+
+  // memory address of variables
+  cout << "\n-- address of both x and xref --\n";
+  cout << "x address: " << &x << endl;
+  cout << "xref address: " << &xref << endl;
+
+  cout << "\n-- using reference to change vector --\n";
+  vector<string> colors{"red", "yellow", "blue", "white"};
+
+  // replace white to black
+  cout << "\nvector before reassignment using values: " << endl;
+  display_vector(colors);
+
+  for (auto str : colors) // values used
+  {
+    if (str == "white")
+    {
+      str = "black";
+    }
+  }
+
+  cout << "\nvector after reassignment using values: " << endl;
+  display_vector(colors);
+
+  for (auto &str : colors) // references used
+  {
+    if (str == "white")
+    {
+      str = "black";
+    }
+  }
+
+  cout << "\nvector after reassignment using references: " << endl;
+  display_vector(colors);
 
   return 0;
+}
+
+void display_vector(vector<string> vec)
+{
+  for (auto str : vec)
+  {
+    cout << str << " ";
+  }
+  cout << endl;
 }
