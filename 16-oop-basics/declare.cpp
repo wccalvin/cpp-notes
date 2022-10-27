e5aa968a4e201dec0296b7867d7ebf5b11c17579
wccalvin <clayton.calvin@gmail.com>
2022-10-27 12:09:44 -0400

    class - accessing members

--- 16-oop-basics/declare.cpp
+++ 16-oop-basics/declare.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 class Player
 {
+public:
   // attributes
-  string name;
-  int health;
-  int xp;
+  string name{};
+  int health{};
+  int xp{};
 
   // methods
   void talk(string); // function prototypes
@@ -18,8 +19,9 @@ class Player
 
 class Account
 {
-  string name;
-  double balance;
+public:
+  string name{};
+  double balance{};
 
   bool deposit(double);
   bool withdraw(double);
@@ -30,21 +32,38 @@ int main()
   cout << "-- create objects in stack - players--\n";
   // objects on the stack
   Player player_a;
-  Player player_b;
+
+  // accessing class members to set values
+  player_a.name = "clayton";
+  player_a.health = 100;
+  player_a.xp = 9;
+
+  cout << "player_a.name: " << player_a.name << endl;
+  cout << "player_a.health: " << player_a.health << endl;
+  cout << "player_a.xp: " << player_a.xp << endl;
 
   // create user defined types using class objects
-  Player player_array[]{player_a, player_b};
+  Player player_array[]{player_a};
 
   vector<Player> player_vector{player_a};
+  Player player_b;
   player_vector.push_back(player_b);
 
   // object on the heap
   cout << "\n-- create objects in heap --\n";
   Player *player_c{nullptr};
   player_c = new Player;
+  player_c->name = "john";
+  player_c->health = 98;
+  player_c->xp = 7;
+
+  cout << "player_c.name: " << player_c->name << endl;
+  cout << "player_c.health: " << player_c->health << endl;
+  cout << "player_c.xp: " << player_c->xp << endl;
+
   delete player_c;
 
-  cout << "-- create objects in stack - accounts--\n";
+  cout << "\n-- create objects in stack - accounts--\n";
   Account account_a;
   Account account_b;
 
