7d3bad18cda254bb16904354888d835bdf79a967
wccalvin <clayton.calvin@gmail.com>
2023-01-11 09:05:37 -0500

    add get methods

--- 17-oop-constructors/using_constructor_initialization.cpp
+++ 17-oop-constructors/using_constructor_initialization.cpp
@@ -13,6 +13,8 @@ public:
   Dog();
   Dog(string n);
   Dog(string n, int a);
+  string getDogName();
+  int getDogAge();
 };
 
 // using constructor initialization instead of reassigning
@@ -25,9 +27,15 @@ Dog::Dog(string n)
 Dog::Dog(string n, int a)
     : name{n}, age{a} {}
 
+string Dog::getDogName() { return name; }
+int Dog::getDogAge() { return age; }
+
 int main()
 {
   Dog dog_a;
   Dog dog_b("halo");
   Dog dog_c("rover", 2);
+
+  cout << dog_c.getDogName() << " the dog is " << dog_c.getDogAge() << " years old.\n";
+  return 0;
 }
