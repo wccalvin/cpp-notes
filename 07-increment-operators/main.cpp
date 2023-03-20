4b4e0a97a7c862435f71ea4e269e120d1df2982b
wccalvin <clayton.calvin@gmail.com>
2023-03-20 09:55:26 -0400

    revamp increment example

--- 07-increment-operators/main.cpp
+++ 07-increment-operators/main.cpp
@@ -10,22 +10,23 @@
 int main()
 {
   // pre-increment example
+  std::cout << "-----------------" << std::endl;
+  std::cout << "Example of pre-increment ++x" << std::endl;
   int x = 5;
-  std::cout << "x before pre-increment: " << x << std::endl;
-
+  std::cout << "x = " << x << std::endl;
   int y = ++x;
-  std::cout << "x after pre-increment: " << x << std::endl;
-  std::cout << "y after pre-increment: " << y << std::endl;
-  std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "x = " << x << std::endl;
+  std::cout << "y = " << y << std::endl;
+  std::cout << "-----------------" << std::endl;
 
   // post-increment example
-  int a = 5;
-  std::cout << "a before post-increment: " << a << std::endl;
-
-  int b = a++;
-  std::cout << "a after post-increment: " << a << std::endl;
-  std::cout << "b after post-increment: " << b << std::endl;
-  std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "-----------------" << std::endl;
+  std::cout << "Example of post-increment x++" << std::endl;
+  std::cout << "x = " << x << std::endl;
+  y = x++;
+  std::cout << "x = " << x << std::endl;
+  std::cout << "y = " << y << std::endl;
+  std::cout << "-----------------" << std::endl;
 
   return 0;
 }
