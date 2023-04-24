27cb3e0cb68d63107bcdd5c2af0c1e28595bc209
wccalvin <clayton.calvin@gmail.com>
2023-04-24 08:46:39 -0400

    example of sizeof

---
+++ 01-hello/sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Size of int: " << sizeof(int) << " bytes." << endl;
+  cout << "Size of unsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+  cout << "Size of short: " << sizeof(short) << " bytes." << endl;
+  cout << "Size of float: " << sizeof(float) << " bytes." << endl;
+  cout << "Size of double: " << sizeof(double) << " bytes." << endl;
+  cout << "Size of bool: " << sizeof(bool) << " bytes." << endl;
+  cout << "Size of long int: " << sizeof(long int) << " bytes." << endl;
+  return 0;
+}
