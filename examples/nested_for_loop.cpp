cd28fe6affbfcce1191898ef0faea4fa0a4b916c
wccalvin <clayton.calvin@gmail.com>
2023-04-27 13:14:23 -0400

    add a new example

---
+++ examples/nested_for_loop.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows = 6;
+  int cols = 6;
+
+  float matrix[rows][cols] = {
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+  };
+
+  // your code goes here
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      matrix[i][j] = 1;
+    }
+  }
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      cout << matrix[i][j];
+    }
+    cout << endl;
+  }
+}
